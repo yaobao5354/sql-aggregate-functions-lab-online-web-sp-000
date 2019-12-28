8879ee4c790160613f6951e864e11ae758b165ca
yaoyu bao <yaobao5354@gmail.com>
2019-12-28 01:13:45 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  SELECT * FROM cats ORDER BY age DESC LIMIT 1;
+  SELECT * FROM students ORDER BY gpa DESC LIMIT 1;
 end
 
 def lowest_student_gpa
